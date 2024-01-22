31306f03afe0d6bae52367ed52cf50b2793273a9
Ivana Spasova <zlwf168@live.rhul.ac.uk>
2024-01-22 14:15:42 +0000

    Update sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+/*************
+ Variables
+I will experiment with :
+- Two circles in different colours;
+- Changing width and height;
+- background ()
+- fill ()
+- circle ()
+- r //that means radius
+ ************/
+
